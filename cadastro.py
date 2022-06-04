261dac411c0198f2228349c82c225b4a79a59500
Artur_Dev? <106828444+TurSilv4@users.noreply.github.com>
2022-06-04 12:26:36 -0300

    Update cadastro.py

--- cadastro.py
+++ cadastro.py
@@ -1,4 +1,5 @@
 from datetime import date
+import getpass
 
 base_dados = list()
 login_senha = list()
@@ -10,11 +11,10 @@ user['Ano_nascimento'] = int(input('Ano de nascimento: '))
 user['Idade'] = date.today().year - user['Ano_nascimento']
 user['Email'] = str(input('Email de cadastro: ')).strip().lower()
 user['Telefone'] = int(input('Número de telefone: '))
-user_login['Login'] = str(input('Nome de login: '))
-user_login['Senha'] = str(input('Senha: '))
+user_login['Login'] = getpass.getuser('Login: ')
+user_login['Senha'] = getpass.getpass('Password: ')
 login_senha.append(user_login.copy())
 user_login.clear()
 base_dados.append(user.copy())
 user.clear()
-print(login_senha)
-print(base_dados)
+
